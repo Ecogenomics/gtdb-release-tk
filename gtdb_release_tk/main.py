38063b0640f0b40913bc1fe7d267e93007e55017
pchaumeil <uqpchaum@uq.edu.au>
2019-06-14 15:26:44 +1000

    add taxonomy comparison functionality.

--- gtdb_release_tk/main.py
+++ gtdb_release_tk/main.py
@@ -27,25 +27,26 @@ from biolib.common import check_file_exists, make_sure_path_exists
 from gtdb_release_tk.website_data import WebsiteData
 from gtdb_release_tk.plots.genome_types_per_rank import GenomeTypesPerRank
 
+
 class OptionsParser():
     def __init__(self):
         """Initialization"""
-        
+
         self.logger = logging.getLogger('timestamp')
-        
+
     def taxonomy_files(self, options):
         """Generate taxonomy files for GTDB website."""
 
         check_file_exists(options.metadata_file)
         check_file_exists(options.user_gid_table)
         make_sure_path_exists(options.output_dir)
-        
+
         p = WebsiteData(options.release_number, options.output_dir)
         p.taxonomy_files(options.metadata_file,
-                            options.user_gid_table)
+                         options.user_gid_table)
 
         self.logger.info('Done.')
-        
+
     def tree_files(self, options):
         """Generate tree files spanning representative genomes."""
 
@@ -53,14 +54,14 @@ class OptionsParser():
         check_file_exists(options.ar_tree)
         check_file_exists(options.user_gid_table)
         make_sure_path_exists(options.output_dir)
-        
+
         p = WebsiteData(options.release_number, options.output_dir)
         p.tree_files(options.bac_tree,
-                            options.ar_tree,
-                            options.user_gid_table)
+                     options.ar_tree,
+                     options.user_gid_table)
 
         self.logger.info('Done.')
-        
+
     def sp_cluster_file(self, options):
         """Generate file indicating GTDB species clusters."""
 
@@ -68,11 +69,11 @@ class OptionsParser():
         check_file_exists(options.gtdb_sp_clusters_file)
         check_file_exists(options.user_gid_table)
         make_sure_path_exists(options.output_dir)
-        
+
         p = WebsiteData(options.release_number, options.output_dir)
         p.sp_cluster_file(options.metadata_file,
-                            options.gtdb_sp_clusters_file,
-                            options.user_gid_table)
+                          options.gtdb_sp_clusters_file,
+                          options.user_gid_table)
 
         self.logger.info('Done.')
 
@@ -84,24 +85,50 @@ class OptionsParser():
         check_file_exists(options.user_gid_table)
         check_file_exists(options.genome_path_file)
         make_sure_path_exists(options.output_dir)
-        
+
         p = WebsiteData(options.release_number, options.output_dir)
         p.ssu_files(options.metadata_file,
-                        options.gtdb_sp_clusters_file,
-                        options.user_gid_table,
-                        options.genome_path_file,
-                        options.min_16S_ar_len,
-                        options.min_16S_bac_len,
-                        options.min_contig_len)
+                    options.gtdb_sp_clusters_file,
+                    options.user_gid_table,
+                    options.genome_path_file,
+                    options.min_16S_ar_len,
+                    options.min_16S_bac_len,
+                    options.min_contig_len)
 
         self.logger.info('Done.')
+
+    def tax_comp_files(self, options):
+        """Generate comparison files between 2 taxonomy ordered by degree of polyphyly."""
+        check_file_exists(options.reference_taxonomy_file)
+        check_file_exists(options.new_taxonomy_file)
+        make_sure_path_exists(options.output_dir)
+
+        p = WebsiteData('', options.output_dir)
+        p.tax_comp_files(options.reference_taxonomy_file,
+                         options.new_taxonomy_file,
+                         options.user_gid_table,
+                         options.keep_no_change,
+                         options.keep_placeholder_name,
+                         options.top_change)
+
+        self.logger.info('Done.')
+
+    def json_tree_parser(self, options):
+        """generate Json file used to create the tree in http://gtdb.ecogenomic.org/tree"""
+        print('in DEV')
+        self.logger.info('Done.')
+
+        p = WebsiteData(options.release_number, options.output_dir)
+        p.json_tree_parser(options.taxonomy_file,
+                           options.metadata_file,
+                           options.user_gid_table)
 
     def genome_types_per_rank(self, options):
         """Plot number of MAGs, SAGs, and isolates for each taxonomic rank."""
 
         check_file_exists(options.metadata_file)
         make_sure_path_exists(options.output_dir)
-        
+
         p = GenomeTypesPerRank(options.output_dir)
         p.run(options.metadata_file)
 
@@ -120,8 +147,13 @@ class OptionsParser():
             self.ssu_files(options)
         elif options.subparser_name == 'genome_types_per_rank':
             self.genome_types_per_rank(options)
+        elif options.subparser_name == 'tax_comp_files':
+            self.tax_comp_files(options)
+        elif options.subparser_name == 'json_tree_parser':
+            self.json_tree_parser(options)
         else:
-            self.logger.error('Unknown command: ' + options.subparser_name + '\n')
+            self.logger.error('Unknown command: ' +
+                              options.subparser_name + '\n')
             sys.exit()
 
         return 0
